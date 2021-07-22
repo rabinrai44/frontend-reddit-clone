feb88c2df5c04a8006cde0d5caf60b98bffa734d
Rabin Rai <rabinrai44@gmail.com>
2021-07-22 00:39:10 -0400

    Added Signup route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    LoginComponent
+    SignupComponent
   ],
   imports: [
     BrowserModule,
